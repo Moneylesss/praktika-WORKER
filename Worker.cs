b7b329912688822ad55e2de5244ddbbb9edae39a
Moneylesss <134306928+Moneylesss@users.noreply.github.com>
2026-01-14 22:52:49 +0500

    Update Worker.cs

--- Worker.cs
+++ Worker.cs
@@ -2,13 +2,11 @@ using System;
 
 public class Worker
 {
-    // Поля класса
     private string fio;
     private string position;
     private decimal salary;
     private int yearStartWork;
     
-    // Конструктор по умолчанию
     public Worker()
     {
         fio = "Не указано";
@@ -17,7 +15,6 @@ public class Worker
         yearStartWork = 0;
     }
     
-    // Конструктор с параметрами
     public Worker(string fio, string position, decimal salary, int yearStartWork)
     {
         this.fio = fio;
@@ -26,13 +23,11 @@ public class Worker
         this.yearStartWork = yearStartWork;
     }
     
-    // Деструктор
     ~Worker()
     {
         Console.WriteLine($"Объект {fio} уничтожен");
     }
     
-    // Свойства для доступа к полям
     public string FIO 
     {
         get { return fio; }
@@ -57,7 +52,6 @@ public class Worker
         set { yearStartWork = value; }
     }
     
-    // Метод для ввода данных
     public void InputData()
     {
         Console.Write("Введите ФИО: ");
@@ -73,7 +67,6 @@ public class Worker
         yearStartWork = int.Parse(Console.ReadLine());
     }
     
-    // Метод для отображения данных
     public void ShowInfo()
     {
         Console.WriteLine($"ФИО: {fio}");
@@ -82,7 +75,6 @@ public class Worker
         Console.WriteLine($"Год начала работы: {yearStartWork}");
     }
     
-    // Метод расчета стажа
     public int GetExperience()
     {
         return DateTime.Now.Year - yearStartWork;
